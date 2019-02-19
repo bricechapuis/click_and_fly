c7fa7ebcf65c229d14134d42fcac899853321180
Benoit Roualec <benoit.roualec@gmail.com>
2019-02-19 11:24:39 +0100

    Access for Bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,5 +2,6 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.where(user_id: current_user.id)
     @bookings_for_my_planes = Booking.where(plane_id: current_user.planes.ids[0])
+    @planes = policy_scope(Plane).order(created_at: :desc)
   end
 end
