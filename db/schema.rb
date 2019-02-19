96e4823514bd2199714337b338230a2ba9886d33
Guillaume Ecollan <guillaume@ecollan.fr>
2019-02-19 12:38:30 +0100

    close

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_094717) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.integer "phone_number"
-    t.boolean "admin", default: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
