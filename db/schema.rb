7f3b5bc635dae8306de89c22c601011f33086eb8
Yahya Fallah <yahya.fallah.araghi@gmail.com>
2019-02-19 15:50:11 +0100

    schema push

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_02_19_095632) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,7 +53,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_095632) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.integer "phone_number"
-    t.boolean "admin", default: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
