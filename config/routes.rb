f735bc7ffd5f197adbaca2ed7c6f33adb874f96b
Guillaume Ecollan <guillaume@ecollan.fr>
2019-02-19 16:41:18 +0100

    decline or confirm works on a bookind

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :planes do
-    resources :bookings, only: [:show, :new, :create, :edit, :update]
+    resources :bookings, only: [:show, :new, :create, :edit, :update] do 
+    end
   end
   get '/bookings', to: 'bookings#index', as: :bookings
+  patch 'bookings/:id/decline', to: 'bookings#decline_booking', as: :decline
+  patch 'bookings/:id/confirm', to: 'bookings#confirm_booking', as: :confirm
 end
