ec6f47ce7b279f30257c1b200a0adde0ffb32adc
Brice Chapuis <brice.chapuis@gmail.com>
2019-02-20 15:26:08 +0100

    navbar update

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get '/bookings', to: 'bookings#index', as: :bookings
   patch 'bookings/:id/decline', to: 'bookings#decline_booking', as: :decline
   patch 'bookings/:id/confirm', to: 'bookings#confirm_booking', as: :confirm
+  get 'user/:id/my_planes', to: 'users#my_planes', as: :myplanes
 end
