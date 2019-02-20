ccb22e5f68492e37c8ec4712ee6038b50d1f55e6
Guillaume Ecollan <guillaume@ecollan.fr>
2019-02-20 12:14:41 +0100

    map for each plane

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,8 @@
 import "bootstrap";
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+
+import { initMapbox } from '../components/init_mapbox.js';
+initMapbox();
+
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_152616) do
+ActiveRecord::Schema.define(version: 2019_02_20_091603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2019_02_19_152616) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_planes_on_user_id"
   end
 
@@ -54,7 +56,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_152616) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.integer "phone_number"
-    t.boolean "admin", default: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
