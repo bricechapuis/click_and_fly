aaa03bb02f5a5b31a276c494cba13c8932f67d38
Yahya Fallah <yahya.fallah.araghi@gmail.com>
2019-02-22 15:27:53 +0100

    user added to review

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :planes
   has_many :bookings
+  has_many :reviews
 
   mount_uploader :photo, PhotoUploader
 end
